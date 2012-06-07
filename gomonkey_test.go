9c305639c3e46df2e217c14da312df3e95057806
Martin Bruse <martin@oort.se>
2012-06-07 15:59:13 +0200

    more tests

--- gomonkey_test.go
+++ gomonkey_test.go
@@ -3,13 +3,32 @@ package gomonkey
 
 import (
 	"testing"
+	"reflect"
 )
 
 func TestBasicEvaluation(t *testing.T) {
 	script := NewJS()
+	defer script.Destroy()
 	x := script.Eval("x = 10; x * x;")
 	if x != 100.0 {
-		t.Error(x,"Should be 100")
+		t.Error(x, "Should be 100")
 	}
-	script.Destroy()
+}
+
+func TestObjectReturn(t *testing.T) {
+	script := NewJS()
+	defer script.Destroy()
+	x := script.Eval("new Object()")
+	if reflect.TypeOf(x) != reflect.TypeOf(&JSObject{}) {
+		t.Error(x, "Should be a JSObject")
+	}
+}
+
+func TestFunctionReturn(t *testing.T) {
+	script := NewJS()
+	defer script.Destroy()
+	x := script.Eval("function() { return 1; }")
+	if reflect.TypeOf(x) != reflect.TypeOf(&JSFunction{}) {
+		t.Error(x, "Should be a JSFunction")
+	}
 }
